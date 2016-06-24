5cdd9e61d85680a94108e4ede267da0bd7135dca
Yuri Shkuro <ys@uber.com>
2016-06-23 23:12:30 -0400

    Support pluggable injectors/extrators; implement Zipkin propagator

---
+++ zipkin.go
@@ -0,0 +1,75 @@
+package jaeger
+
+import (
+	"github.com/opentracing/opentracing-go"
+)
+
+// ZipkinSpanFormat is an OpenTracing carrier format constant
+const ZipkinSpanFormat = "zipkin-span-format"
+
+// ZipkinSpan is a type of Carrier used for integration with Zipkin-aware RPC frameworks
+// (like TChannel). It does not support baggage, only trace IDs.
+type ZipkinSpan interface {
+	TraceID() uint64
+	SpanID() uint64
+	ParentID() uint64
+	Flags() byte
+	SetTraceID(traceID uint64)
+	SetSpanID(spanID uint64)
+	SetParentID(parentID uint64)
+	SetFlags(flags byte)
+}
+
+type zipkinPropagator struct {
+	tracer *tracer
+}
+
+func (p *zipkinPropagator) InjectSpan(
+	sp opentracing.Span,
+	abstractCarrier interface{},
+) error {
+	sc, ok := sp.(*span)
+	if !ok {
+		return opentracing.ErrInvalidSpan
+	}
+	carrier, ok := abstractCarrier.(ZipkinSpan)
+	if !ok {
+		return opentracing.ErrInvalidCarrier
+	}
+
+	sc.RLock()
+	defer sc.RUnlock()
+
+	carrier.SetTraceID(sc.TraceContext.TraceID())
+	carrier.SetSpanID(sc.TraceContext.SpanID())
+	carrier.SetParentID(sc.TraceContext.ParentID())
+	carrier.SetFlags(sc.TraceContext.flags)
+
+	return nil
+}
+
+func (p *zipkinPropagator) Join(
+	operationName string,
+	abstractCarrier interface{},
+) (opentracing.Span, error) {
+	carrier, ok := abstractCarrier.(ZipkinSpan)
+	if !ok {
+		return nil, opentracing.ErrInvalidCarrier
+	}
+	if carrier.TraceID() == 0 {
+		return nil, opentracing.ErrTraceNotFound
+	}
+	sp := p.tracer.newSpan()
+	sp.TraceContext.traceID = carrier.TraceID()
+	sp.TraceContext.spanID = carrier.SpanID()
+	sp.TraceContext.parentID = carrier.ParentID()
+	sp.TraceContext.flags = carrier.Flags()
+
+	return p.tracer.startSpanInternal(
+		sp,
+		operationName,
+		p.tracer.timeNow(),
+		nil,
+		true, // join with external trace
+	), nil
+}
