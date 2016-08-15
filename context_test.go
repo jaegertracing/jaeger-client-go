587d86358e8932fb0b2233a52023607efa41c133
Yuri Shkuro <ys@uber.com>
2016-08-15 18:31:17 -0400

    fmt

--- context_test.go
+++ context_test.go
@@ -3,8 +3,8 @@ package jaeger
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestContextFromString(t *testing.T) {
