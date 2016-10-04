a9b6ed01dd4faeb94c977a5dbf0a10fe2ec8f19e
Yuri Shkuro <ys@uber.com>
2016-10-04 17:46:10 -0400

    Add package doc

--- transport/doc.go
+++ transport/doc.go
@@ -3,4 +3,7 @@
 // for serializing the spans into a specific format suitable for sending
 // to the tracing backend. Examples may include Thrift over UDP, Thrift
 // or JSON over HTTP, Thrift over Kafka, etc.
+//
+// Implementations are NOT required to be thread-safe; the RemoteReporter
+// is expected to only call methods on the Transport from the same go-routine.
 package transport
