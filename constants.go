16278cd23777cb928243f99e29f3d097d21bc749
Won Jun Jang <wonjun.jangobe@gmail.com>
2017-02-09 12:03:43 -0500

    Update JaegerClientVersion to 2.0.0 (#89)

--- constants.go
+++ constants.go
@@ -22,7 +22,7 @@ package jaeger
 
 const (
 	// JaegerClientVersion is the version of the client library reported as Span tag.
-	JaegerClientVersion = "Go-1.6"
+	JaegerClientVersion = "Go-2.0.0"
 
 	// JaegerClientVersionTagKey is the name of the tag used to report client version.
 	JaegerClientVersionTagKey = "jaeger.version"
