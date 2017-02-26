f29faa0ca24418d2ddfaca103c689a3af055ee3e
Won Jun Jang <wjang@uber.com>
2017-02-25 22:41:06 -0500

    Address comments

--- config/example_test.go
+++ config/example_test.go
@@ -26,69 +26,24 @@ import (
 	"github.com/uber/jaeger-lib/metrics"
 
 	"github.com/uber/jaeger-client-go"
-	. "github.com/uber/jaeger-client-go/config"
-	jaeger_log "github.com/uber/jaeger-client-go/log"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	jaegerlog "github.com/uber/jaeger-client-go/log"
 )
 
 func ExampleInitialization() {
-	cfg := Configuration{
-		Disabled: false,
-		Sampler: &SamplerConfig{
+	cfg := jaegercfg.Configuration{
+		Sampler: &jaegercfg.SamplerConfig{
 			Type:  jaeger.SamplerTypeProbabilistic,
 			Param: 0.001,
 		},
 	}
 
 	// Initialize tracer with a logger and a metrics factory
-	closer, err := cfg.InitGlobalTracer("serviceName", Logger(newLogger()), Metrics(newMetricsFactory()))
+	closer, err := cfg.InitGlobalTracer("serviceName", jaegercfg.Logger(jaegerlog.StdLogger),
+		jaegercfg.Metrics(metrics.NullFactory))
 	if err != nil {
 		log.Printf("Could not initialize jaeger tracer: %s", err.Error())
 		return
 	}
 	defer closer.Close()
-}
-
-// yourLogger is an implementation of the jaeger-client-go/log#Logger interface that
-// delegates to default `log` package.
-// jaeger-client-go has a Logger wrapper for zap ("github.com/uber/jaeger-client-go/log/zap")
-type yourLogger struct{}
-
-func newLogger() jaeger_log.Logger {
-	return &yourLogger{}
-}
-
-// Error logs a message at error priority
-func (l *yourLogger) Error(msg string) {
-	log.Printf("ERROR: %s", msg)
-}
-
-// Infof logs a message at info priority
-func (l *yourLogger) Infof(msg string, args ...interface{}) {
-	log.Printf(msg, args...)
-}
-
-// yourMetricsFactory is an implementation of jaeger-lib/metrics#Factory that returns
-// NullCounter, NullTimer, and NullGauge.
-// jaeger-lib has metrics factories for go-kit ("github.com/uber/jaeger-lib/metrics/go-kit") and
-// tally ("github.com/uber/jaeger-lib/metrics/tally")
-type yourMetricsFactory struct{}
-
-func newMetricsFactory() metrics.Factory {
-	return &yourMetricsFactory{}
-}
-
-func (yourMetricsFactory) Counter(name string, tags map[string]string) metrics.Counter {
-	return metrics.NullCounter
-}
-
-func (yourMetricsFactory) Timer(name string, tags map[string]string) metrics.Timer {
-	return metrics.NullTimer
-}
-
-func (yourMetricsFactory) Gauge(name string, tags map[string]string) metrics.Gauge {
-	return metrics.NullGauge
-}
-
-func (yourMetricsFactory) Namespace(name string, tags map[string]string) metrics.Factory {
-	return metrics.NullFactory
 }
