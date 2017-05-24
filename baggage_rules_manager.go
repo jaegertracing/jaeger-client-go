7e2768e519f939bf263478a95bc8843cddb5c89a
Won Jun Jang <wjang@uber.com>
2017-05-24 17:00:58 -0400

    baggage rules

--- baggage_rules_manager.go
+++ baggage_rules_manager.go
@@ -1,3 +1,23 @@
+// Copyright (c) 2017 Uber Technologies, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 package jaeger
 
 import (
@@ -18,22 +38,46 @@ type BaggageRulesManager interface {
 type baggageRulesManager struct {
 	sync.RWMutex
 
+	serviceName string
 	baggageSize map[string]int32
-	timer *time.Ticker
-	manager     baggage.Baggage
+	timer       *time.Ticker
+	manager     baggage.BaggageManager
 	pollStopped sync.WaitGroup
 }
 
-func NewBaggageRulesManager() BaggageRulesManager {
+func NewBaggageRulesManager(serviceName string, manager baggage.BaggageManager) BaggageRulesManager {
 	m := &baggageRulesManager{
+		serviceName: serviceName,
 		baggageSize: make(map[string]int32),
-		timer:          time.NewTicker(defaultRefreshInterval),
+		timer:       time.NewTicker(defaultRefreshInterval),
+		manager:     manager,
 	}
-	go m.pollController()
+	go m.pollManager()
 	return m
 }
 
-func (m *baggageRulesManager) pollController() {
+func (m *baggageRulesManager) IsValidBaggageKey(key string) (bool, int32) {
+	m.RLock()
+	defer m.RUnlock()
+	if size, ok := m.baggageSize[key]; ok {
+		return true, size
+	}
+	return false, 0
+}
+
+// Close implements Close() of Sampler.
+func (m *baggageRulesManager) Close() {
+	m.Lock()
+	m.timer.Stop()
+	m.Unlock()
+
+	m.pollStopped.Wait()
+}
+
+func (m *baggageRulesManager) pollManager() {
+	m.pollStopped.Add(1)
+	defer m.pollStopped.Done()
+
 	// in unit tests we re-assign the timer Ticker, so need to lock to avoid data races
 	m.Lock()
 	timer := m.timer
@@ -42,38 +86,26 @@ func (m *baggageRulesManager) pollController() {
 	for range timer.C {
 		m.updateRules()
 	}
-	//s.pollStopped.Add(1)
 }
 
 func (m *baggageRulesManager) updateRules() {
-
-	GetBaggageRules(serviceName string) (r []*BaggageRule, err error)
-
-	//res, err := s.manager.GetSamplingStrategy(s.serviceName)
-	//if err != nil {
-	//	s.metrics.SamplerQueryFailure.Inc(1)
-	//	return
-	//}
-	//s.Lock()
-	//defer s.Unlock()
-	//
-	//s.metrics.SamplerRetrieved.Inc(1)
-	//if strategies := res.GetOperationSampling(); strategies != nil {
-	//	s.updateAdaptiveSampler(strategies)
-	//} else {
-	//	err = s.updateRateLimitingOrProbabilisticSampler(res)
-	//}
-	//if err != nil {
-	//	s.metrics.SamplerUpdateFailure.Inc(1)
-	//	s.logger.Infof("Unable to handle sampling strategy response %+v. Got error: %v", res, err)
-	//	return
-	//}
-	//s.metrics.SamplerUpdated.Inc(1)
-}
-
-func (m *baggageRulesManager) Init() error {
+	rules, err := m.manager.GetBaggageRules(m.serviceName)
+	if err != nil {
+		// TODO update failure metric
+		// TODO this is pretty serious, almost retry worthy
+		return
+	}
+	rulesMap := convertRulesToMap(rules)
 	m.Lock()
 	defer m.Unlock()
-	// TODO get the baggage rules from agent
-	return nil
+	m.baggageSize = rulesMap
+	// TODO update metrics
+}
+
+func convertRulesToMap(rules []*baggage.BaggageRule) map[string]int32 {
+	rulesMap := make(map[string]int32, len(rules))
+	for _, rule := range rules {
+		rulesMap[rule.Key] = rule.Size
+	}
+	return rulesMap
 }
