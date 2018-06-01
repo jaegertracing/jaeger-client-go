65e76abf672c8a7d037047ec8d9e0211dc3444ae
Won Jun Jang <wjang@uber.com>
2018-06-01 17:17:37 -0400

    upperbound
    Signed-off-by: Won Jun Jang <wjang@uber.com>

--- adaptive_sampler.go
+++ adaptive_sampler.go
@@ -18,19 +18,23 @@ import (
 	"fmt"
 	"sync"
 	"time"
+	"github.com/uber/jaeger-client-go/utils"
+	"math"
 )
 
 const (
 	defaultProbability = 0.001
 )
 
-type data struct {
+// struct for debugging purposes only, keeps track of throughput and number of samples
+type metadata struct {
 	throughput int64
 	samples    int64
 }
 
 type AdaptiveSampler struct {
 	samplers            map[string]*ProbabilisticSampler
+	upperbound          map[string]utils.RateLimiter
 	targetQPS           float64
 	probabilities       map[string]float64
 	samples             map[string]int64
@@ -38,7 +42,7 @@ type AdaptiveSampler struct {
 	mux                 sync.Mutex
 	calculationInterval time.Duration
 
-	totalSampled map[string]*data
+	totalSampled map[string]*metadata
 	start        time.Time
 }
 
@@ -46,11 +50,12 @@ func NewAdaptiveSamplerV2(targetQPS float64) (*AdaptiveSampler, error) {
 	sampler := &AdaptiveSampler{
 		samples:             make(map[string]int64),
 		samplers:            make(map[string]*ProbabilisticSampler),
+		upperbound:          make(map[string]utils.RateLimiter),
 		probabilities:       make(map[string]float64),
 		targetQPS:           targetQPS,
 		calculationInterval: time.Duration(1000000.0/targetQPS) * time.Microsecond * 2, // Nyquist rate
 		start:               time.Now(),
-		totalSampled:        make(map[string]*data),
+		totalSampled:        make(map[string]*metadata),
 	}
 	go sampler.run()
 	go sampler.log()
@@ -65,8 +70,8 @@ func (a *AdaptiveSampler) run() {
 		select {
 		case <-ticker.C:
 			a.calculate()
-			//case <-t.close:
-			//	return
+		//case <-t.close:
+		//	return
 		}
 	}
 }
@@ -78,7 +83,10 @@ func (a *AdaptiveSampler) log() {
 		select {
 		case <-ticker.C:
 			a.mux.Lock()
-			print(fmt.Sprintf("duration: %v, counts: %v", time.Now().Sub(a.start), a.totalSampled))
+			print(fmt.Sprintf("duration: %v\n", time.Now().Sub(a.start)))
+			for operation, d := range a.totalSampled {
+				print(fmt.Sprintf("operation: %s throughput: %v samples: %v\n", operation, d.throughput, d.samples))
+			}
 			a.mux.Unlock()
 		}
 	}
@@ -89,7 +97,9 @@ func (a *AdaptiveSampler) IsSampled(id TraceID, operation string) (bool, []Tag) 
 	defer a.mux.Unlock()
 	if _, ok := a.samples[operation]; !ok {
 		a.samples[operation] = 0
-		a.totalSampled[operation] = &data{}
+	}
+	if _, ok := a.totalSampled[operation]; !ok {
+		a.totalSampled[operation] = &metadata{}
 	}
 	var sampler *ProbabilisticSampler
 	var ok bool
@@ -97,12 +107,23 @@ func (a *AdaptiveSampler) IsSampled(id TraceID, operation string) (bool, []Tag) 
 		sampler, _ = NewProbabilisticSampler(defaultProbability)
 		a.probabilities[operation] = defaultProbability
 		a.samplers[operation] = sampler
+
+		upperbound := utils.NewRateLimiter(a.targetQPS, math.Max(a.targetQPS, 1.0))
+		a.upperbound[operation] = upperbound
 	}
 	sampled, tags := sampler.IsSampled(id, operation)
 	a.totalSampled[operation].throughput += 1
 	if sampled {
 		a.totalSampled[operation].samples += 1
 		a.samples[operation] += 1
+		upperbound := a.upperbound[operation]
+		if !upperbound.CheckCredit(1.0) {
+			// Can probably do better maths, ie throughput was X in this current window vs throughput for global window (if exists)
+			newProb := a.probabilities[operation] / 2.0
+			sampler, _ = NewProbabilisticSampler(newProb)
+			a.samplers[operation] = sampler
+			print("HALVING PROBABILITY")
+		}
 	}
 	return sampled, tags
 }
@@ -110,15 +131,14 @@ func (a *AdaptiveSampler) IsSampled(id TraceID, operation string) (bool, []Tag) 
 func (a *AdaptiveSampler) calculate() {
 	a.mux.Lock()
 	defer a.mux.Unlock()
-	copy := a.samples
-	a.samples = make(map[string]int64)
 
 	now := time.Now()
 	interval := float64(now.Sub(a.lastTick)) / float64(time.Second)
 
-	for operation, samples := range copy {
+	// Iterate over samplers since a operation may not have been sampled recently
+	for operation, _ := range a.samplers {
 		factor := 2.0
-		if samples != 0 {
+		if samples, ok := a.samples[operation]; ok && samples != 0 {
 			observed := float64(samples) / interval
 			factor = a.targetQPS / observed
 		}
@@ -128,6 +148,7 @@ func (a *AdaptiveSampler) calculate() {
 		a.samplers[operation] = newProbabilisticSampler(a.probabilities[operation])
 	}
 	a.lastTick = now
+	a.samples = make(map[string]int64)
 }
 
 func (a *AdaptiveSampler) Close() {}
@@ -135,3 +156,5 @@ func (a *AdaptiveSampler) Close() {}
 func (a *AdaptiveSampler) Equal(other Sampler) bool {
 	return false
 }
+
+// TODO TTL on old operations, add limit on number of ops
