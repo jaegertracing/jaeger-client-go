ecf2d03a9e8070e6f4ff8fe19247a22f5c650b33
Douglas Reid <douglas-reid@users.noreply.github.com>
2019-02-28 11:08:46 -0800

    Add support for 128bit trace ids to zipkin thrift spans. (#378)
    * Add support for 128bit trace ids to zipkin thrift spans.
    
    Includes:
    - Updating IDL to master
    - Running make thrift
    - Add TraceIDHigh to zipkin_thrift_span
    
    Signed-off-by: Douglas Reid <dougreid@google.com>
    
    * Add bit to test to check for TraceIDHigh
    
    Signed-off-by: Douglas Reid <dougreid@google.com>
    
    * Add separate test of 128bit zipkin thrift
    
    Signed-off-by: Douglas Reid <douglas-reid@users.noreply.github.com>
    
    * Reset idl to commit 3471ffb
    
    Signed-off-by: Douglas Reid <dougreid@google.com>
    
    * Re-run make thrift
    
    Signed-off-by: Douglas Reid <dougreid@google.com>

--- zipkin_thrift_span_test.go
+++ zipkin_thrift_span_test.go
@@ -65,6 +65,38 @@ func TestThriftFirstInProcessSpan(t *testing.T) {
 	}
 }
 
+func Test128bitTraceIDs(t *testing.T) {
+	tracer128, closer128 := NewTracer("OneTwentyEight",
+		NewConstSampler(true),
+		NewNullReporter(),
+		TracerOptions.Gen128Bit(true),
+	)
+	defer closer128.Close()
+
+	tracer64, closer64 := NewTracer("SixtyFour",
+		NewConstSampler(true),
+		NewNullReporter(),
+		TracerOptions.Gen128Bit(false),
+	)
+	defer closer64.Close()
+
+	sp1 := tracer128.StartSpan("s1").(*Span)
+	sp2 := tracer128.StartSpan("sp2", opentracing.ChildOf(sp1.Context())).(*Span)
+	sp2.Finish()
+	sp1.Finish()
+
+	thriftSpan1 := BuildZipkinThrift(sp1)
+	assert.NotNil(t, thriftSpan1.TraceIDHigh)
+
+	thriftSpan2 := BuildZipkinThrift(sp2)
+	assert.NotNil(t, thriftSpan2.TraceIDHigh)
+
+	sp3 := tracer64.StartSpan("s3").(*Span)
+	sp3.Finish()
+	thriftSpan3 := BuildZipkinThrift(sp3)
+	assert.Nil(t, thriftSpan3.TraceIDHigh)
+}
+
 func TestThriftForceSampled(t *testing.T) {
 	tracer, closer := NewTracer("DOOP",
 		NewConstSampler(false), // sample nothing
