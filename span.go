b8197b05185b508c9eb7542b498791d11494fe69
Prithvi Raj <p.r@uber.com>
2019-08-21 16:50:24 -0400

    Update comment
    Signed-off-by: Prithvi Raj <p.r@uber.com>

--- span.go
+++ span.go
@@ -319,7 +319,7 @@ func setSamplingPriority(s *Span, value interface{}) bool {
 	return false
 }
 
-// EnableFirehose sets the firehose tag on the span context
+// EnableFirehose enables firehose flag on the span context
 func EnableFirehose(s *Span) {
 	s.context.flags |= flagFirehose
 }
