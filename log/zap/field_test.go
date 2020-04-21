715cf738811874331741696a5dc614add69bf6f8
Prithvi Raj <vprithvi@users.noreply.github.com>
2020-04-21 17:15:31 -0400

    Add a wrapper to log spans and span interactions (#502)
    - Add zap fields for spans so that they can be pretty logged
    - Add a `LoggingTracer` which produces `LoggingSpan`, which log all interactions with the API. This is intended to help with debugging instrumentation issues.

--- log/zap/field_test.go
+++ log/zap/field_test.go
@@ -17,10 +17,12 @@ package zap
 import (
 	"context"
 	"testing"
+	"time"
 
 	jaeger "github.com/uber/jaeger-client-go"
 
 	opentracing "github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/ext"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
@@ -61,4 +63,91 @@ func withTracedContext(f func(ctx context.Context)) {
 
 	ctx := opentracing.ContextWithSpan(context.Background(), tracer.StartSpan("test"))
 	f(ctx)
+}
+
+func TestSpanField(t *testing.T) {
+	tracer, closer := jaeger.NewTracer("serviceName",
+		jaeger.NewConstSampler(true),
+		jaeger.NewNullReporter(),
+	)
+	defer closer.Close()
+
+	parentSpan := tracer.StartSpan("parent")
+	parentSpanContext := parentSpan.Context().(jaeger.SpanContext)
+	baggageKey := "Peregrin"
+	baggageValue := "Took"
+	parentSpan.SetBaggageItem(baggageKey, baggageValue)
+
+	now := time.Now()
+	// create a child span so that span refs are populated
+	span := tracer.StartSpan("child",
+		opentracing.ChildOf(parentSpan.Context()),
+		opentracing.StartTime(now))
+	spanContext := span.Context().(jaeger.SpanContext)
+
+	// Enable Debug
+	ext.SamplingPriority.Set(span, 1)
+
+	// Enable Firehose
+	jaeger.EnableFirehose(span.(*jaeger.Span))
+
+	// Add a log
+	span.Log(opentracing.LogData{
+		Timestamp: now,
+		Event:     "test",
+	})
+
+	enc := zapcore.NewMapObjectEncoder()
+	Span(span).AddTo(enc)
+
+	assert.Contains(t, enc.Fields, "span")
+	logged, ok := enc.Fields["span"].(map[string]interface{})
+	require.True(t, ok, "Expected map.")
+
+	expected := map[string]interface{}{
+		"context": map[string]interface{}{
+			"trace":    spanContext.TraceID().String(),
+			"span":     spanContext.SpanID().String(),
+			"parent":   parentSpanContext.SpanID().String(),
+			"debug":    true,
+			"sampled":  true,
+			"firehose": true,
+			"baggage": []interface{}{
+				map[string]interface{}{
+					"key":   baggageKey,
+					"value": baggageValue,
+				},
+			},
+		},
+
+		"operation_name": "child",
+		"duration":       time.Duration(0),
+		"start_time":     now,
+		"tags": map[string]interface{}{
+			"key":   "sampling.priority",
+			"value": uint16(1),
+		},
+
+		"logs": []interface{}{
+			map[string]interface{}{
+				"ts": now,
+				"fields": []interface{}{
+					map[string]interface{}{
+						"key":   "event",
+						"value": "test",
+					},
+				},
+			},
+		},
+
+		"span_refs": []interface{}{
+			map[string]interface{}{
+				"type":  "child_of",
+				"span":  parentSpanContext.SpanID().String(),
+				"trace": parentSpanContext.TraceID().String(),
+			},
+		},
+	}
+	assert.Equal(t, expected, logged)
+
 }
